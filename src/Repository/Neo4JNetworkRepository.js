ee981c6da02329d6570be8c1708eb30a9f5dd7e4
leo-dalloz <leo.dalloz@outlook.fr>
2023-02-22 11:49:44 +0100

    Add neo4j request

--- src/Repository/Neo4JNetworkRepository.js
+++ src/Repository/Neo4JNetworkRepository.js
@@ -20,20 +20,111 @@ async function getSalesProductByNetwork(userName) {
 }
     async function getSaleForProductByNetwork(userId, productName) {
         const start = Date.now();
-
-        returnValue["Duration"] = duration;
-        returnValue["Data"] = result;
-        return returnValue;
+        try {
+            const result = await session.run('MATCH (a:Person{name:username})-[r1:followedBy *..4]->(b:Person)-[r2:haveBought]->(c:Product {name:product_name}) WHERE a<>b WITH DISTINCT a, b, c WITH c, COUNT(c) as d RETURN c, d,',
+                {user_name: userName, product_name:productName}
+            )
+            const duration = Date.now() - start;
+            let returnValue = {}
+            returnValue["Duration"] = duration;
+            returnValue["Data"] = result;
+            return returnValue;
+        }
+        catch (e)
+        {
+            throw (e);
+        }
     }
 
 
-    async function getProductVirality(productName) {
+    async function getProductVirality(productName, circleSize) {
         const start = Date.now();
+        try {
+            const result = await session.run('MATCH (n:Person)-[:followedBy *circle_size..circle_size]->(p:Person)-[:haveBought]->(c:Product {name:product_name}) WHERE (n)-[:haveBought]->(c) AND n<>p RETURN p',
+                {product_name:productName,circle_size:circleSize}
+            )
+            const duration = Date.now() - start;
+            let returnValue = {}
+            returnValue["Duration"] = duration;
+            returnValue["Data"] = result;
+            return returnValue;
+        }
+        catch (e)
+        {
+            throw (e);
+        }
+    }
 
+    async function getUserCount() {
+        const start = Date.now();
+        try {
+            const result = await session.run('MATCH (n:Person) RETURN COUNT(n)',
+                {product_name:productName,circle_size:circleSize}
+            )
+            const duration = Date.now() - start;
+            let returnValue = {}
+            returnValue["Duration"] = duration;
+            returnValue["Data"] = result;
+            return returnValue;
+        }
+        catch (e)
+        {
+            throw (e);
+        }
+    }
 
-        returnValue["Duration"] = duration;
-        returnValue["Data"] = result;
-        return returnValue;
+    async function getProductCount() {
+        const start = Date.now();
+        try {
+            const result = await session.run('MATCH (n:Product) RETURN COUNT(n)',
+                {product_name:productName,circle_size:circleSize}
+            )
+            const duration = Date.now() - start;
+            let returnValue = {}
+            returnValue["Duration"] = duration;
+            returnValue["Data"] = result;
+            return returnValue;
+        }
+        catch (e)
+        {
+            throw (e);
+        }
+    }
+
+    async function getFollowersCount() {
+        const start = Date.now();
+        try {
+            const result = await session.run('MATCH (:Person)-[r:followedBy]->(:Person) RETURN COUNT(r)',
+                {product_name:productName,circle_size:circleSize}
+            )
+            const duration = Date.now() - start;
+            let returnValue = {}
+            returnValue["Duration"] = duration;
+            returnValue["Data"] = result;
+            return returnValue;
+        }
+        catch (e)
+        {
+            throw (e);
+        }
+    }
+
+    async function getPurchasedCount() {
+        const start = Date.now();
+        try {
+            const result = await session.run('MATCH (:Person)-[r:havePurchased]->(:Product) RETURN COUNT(r)',
+                {product_name:productName,circle_size:circleSize}
+            )
+            const duration = Date.now() - start;
+            let returnValue = {}
+            returnValue["Duration"] = duration;
+            returnValue["Data"] = result;
+            return returnValue;
+        }
+        catch (e)
+        {
+            throw (e);
+        }
     }
 
 
